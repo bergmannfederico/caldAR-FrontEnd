56661a250600510657f196c7700bd68163dbf94d
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-12-21 16:07:53 -0300

    add-boilers-components

---
+++ src/components/Boilers/Boilers.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from './boilers.module.css';
+
+class Boilers extends Component{
+    render() {
+        return (
+             <div>
+                 <p>
+                     This is a Boilers component
+                 </p>
+             </div>
+        );
+    }
+}
+
+export default Boilers;
