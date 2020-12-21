4ce67fe7764ac1fdcfe6b7cd29cf362772d6ba14
Nahuel <nahuelmartindacri@gmail.com>
2020-12-21 15:05:09 -0300

    add route to technician

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import styles from './navBar.module.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Technician from '../technician';
 
 class NavBar extends Component{
+    
     render() {
-        return (
-             <div className={styles.navBarContainer}>
-                 <ul>
-                     <li>Boilers</li>
-                     <li>Technicians</li>
-                     <li>Buildings</li>
-                     <li>Maintenance services</li>
-                     <li>Customers</li>
-                     <li>Reports</li>
-                     <li>Settings</li>
-                 </ul>
-             </div>
+        const linkStyle = {
+            color: 'white',
+            textDecoration: 'none',
+        }
+            return (
+            <Router>
+                <div className={styles.navBarContainer}>
+                    <ul>
+                        <li>Boilers</li>
+                        <Link style={linkStyle} to='/technician'>
+                            <Route path="/technician" exact component={Technician}/>
+                            <li>Technicians</li>
+                        </Link>
+                        <li>Buildings</li>
+                        <li>Maintenance services</li>
+                        <li>Customers</li>
+                        <li>Reports</li>
+                        <li>Settings</li>
+                    </ul>
+                </div>
+            </Router>
         );
     }
 }
 
-export default NavBar
+export default NavBar;
---
+++ src/components/technician.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Technician extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Technicians</h1>
+            </div>
+        );
+    }
+}
+
+export default Technician;
