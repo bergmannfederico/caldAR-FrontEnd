40ea2ba40b4a59a551745203a4cb2152246d9671
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-12-22 08:44:40 -0300

    add-routes-file

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import MainLayout from './components/MainLayout/MainLayout';
+import Boilers from './components/Boilers'
+
+
+class Routes extends Component{
+  render() {
+      return(
+        <Switch>
+        {//<Redirect exact push from="/" to="/"></Redirect>
+}
+            <Route path="/boilers" >
+                <MainLayout>
+                    <Boilers />
+                </MainLayout>
+            </Route>
+            <Route path="/" >
+                <MainLayout/>
+            </Route>
+        </Switch>
+      );
+  }
+}
+
+export default Routes;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import styles from './navBar.module.css';
+import { Link } from 'react-router-dom';
+
 
 class NavBar extends Component{
+    
     render() {
-        return (
-             <div className={styles.navBarContainer}>
-                 <ul>
-                     <li>Boilers</li>
-                     <li>Technicians</li>
-                     <li>Buildings</li>
-                     <li>Maintenance services</li>
-                     <li>Customers</li>
-                     <li>Reports</li>
-                     <li>Settings</li>
-                 </ul>
-             </div>
+        const linkStyle = {
+            color: 'white',
+            textDecoration: 'none',
+        }
+            return (
+                <div className={styles.navBarContainer}>
+                    <ul>
+                        <li>
+                            <Link style={linkStyle} to='/boilers'>
+                            Boilers
+                            </Link>
+                        </li>
+                        <li>Technicians</li>
+                        <li>Buildings</li>
+                        <li>Maintenance services</li>
+                        <li>Customers</li>
+                        <li>Reports</li>
+                        <li>Settings</li>
+                    </ul>
+                </div>
         );
     }
 }
 
-export default NavBar
+export default NavBar;
+
