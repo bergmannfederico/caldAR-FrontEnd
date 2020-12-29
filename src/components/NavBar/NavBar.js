f3fd1522a7d45b695347a439466ed8aa93a9730e
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-12-28 21:52:34 -0300

    first-commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import styles from './navBar.module.css';
+
+class NavBar extends Component{
+    render() {
+        return (
+             <div className={styles.navBarContainer}>
+                 <ul>
+                     <li>Boilers</li>
+                     <li>Technicians</li>
+                     <li>Buildings</li>
+                     <li>Maintenance services</li>
+                     <li>Customers</li>
+                     <li>Reports</li>
+                     <li>Settings</li>
+                 </ul>
+             </div>
+        );
+    }
+}
+
+export default NavBar
